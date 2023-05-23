955f70f7f43cd38701e1867ddbfcb419d263ade3
techmehedi <ecommehedi@gmail.com>
2023-05-22 23:26:32 -0400

    changes

--- news.js
+++ news.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const searchInput = document.querySelector('.search__input');
         searchInputValue = searchInput.value;
 
-        Userurl = `https://newsapi.org/v2/everything?q=${searchInputValue}&from=2023-05-14&sortBy=popularity&apiKey=28a8dff1341c46be965cb359067b70ab`;
+        Userurl = `https://newsdata.io/api/1/news?apikey=pub_22586d54c0a8cc2132115b8f27c120444300d&q=${searchInputValue}`;
 
         if(searchInputValue.trim() !== ""){
             await main(Userurl);
@@ -28,20 +28,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 });
 
-async function main(url = 'https://newsapi.org/v2/everything?q=news&from=2023-05-14&sortBy=popularity&apiKey=28a8dff1341c46be965cb359067b70ab') {
+
+async function main(url = 'https://newsdata.io/api/1/news?apikey=pub_22586d54c0a8cc2132115b8f27c120444300d&q=chatgpt') {
     const news = await fetch(url);
     const newsData = await news.json();
     console.log(newsData);
     localStorage.setItem('newsData', JSON.stringify(newsData));
     const newsListEl = document.querySelector('.news__container');
-    newsListEl.innerHTML = newsData.articles.map((news, index) => newsHTML(news, index)).join("");
+    newsListEl.innerHTML = newsData.results.map((news, index) => newsHTML(news, index)).join("");
 }
 
 
 function newsHTML(news, index) {
     return `<div class="news" onclick="redirectToNewsPage(${index})">
         <figure class="img">
-            <img src="${news.urlToImage}" alt="" class="news__img">
+            <img src="${news.image_url}" alt="" class="news__img">
         </figure>
         <div class="texts">
             <h1 class="title">${news.title}</h1>
